47c5548efe0e8f20b5a3aa5bd10a7d5cd03caa5a
Thao Dau <96876233+ThaoDau@users.noreply.github.com>
2023-11-07 01:07:19 +0700

    update file bookinghistory

--- src/components/BookingHistoryItem/index.tsx
+++ src/components/BookingHistoryItem/index.tsx
@@ -1,38 +1,103 @@
+import { format } from "date-fns";
 import Swal from "sweetalert2";
-import { IBooking } from "~/interfaces/booking.type";
+// import { IBooking } from "~/interfaces/booking.type";
+import type { DatePickerProps } from 'antd';
+import { DatePicker, Space } from 'antd';
+import { useGetAllBookingByUserIdQuery } from "~/Redux/booking/bookingApi";
 
-const BookingHistoryItem = ({ pitch, status }: IBooking) => {
-    const handleCancelBooking = () => {
-        Swal.fire({
-            position: "center",
-            title: "Warning",
-            text: "Bạn sẽ mất toàn bộ số tiền đặt cọc. Bạn chắc chắn chứ!!",
-            icon: "warning",
-            confirmButtonText: "Đồng ý",
-            showDenyButton: true,
-            returnInputValueOnDeny: false,
-            denyButtonText: "Cancel",
-        }).then((result) => {
-            if (result.isConfirmed) {
-                //    Logic call apii
-            }
-        });
-    };
-    return (
-        <section className="flex items-center space-x-4">
-            <div className="w-10 h-10">
-                <img src={pitch?.avatar} alt="" />
-            </div>
+const BookingHistoryItem = () => {
+  const userId = "65131393f8698962d691cd12";
 
-            <span>{status === "success" ? "Đặt thành công" : "Bạn đã hủy lịch"}</span>
+  const { data: bookingHistory } = useGetAllBookingByUserIdQuery({ user_id: userId });
+  const handleCancelBooking = () => {
+    Swal.fire({
+      position: "center",
+      title: "Warning",
+      text: "Bạn sẽ mất toàn bộ số tiền đặt cọc. Bạn chắc chắn chứ!!",
+      icon: "warning",
+      confirmButtonText: "Đồng ý",
+      showDenyButton: true,
+      returnInputValueOnDeny: false,
+      denyButtonText: "Cancel",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        //    Logic call apii
+      }
+    });
+  };
+  // search 
 
-            {status === "success" && (
-                <button onClick={handleCancelBooking} className="px-4 py-1 bg-green-500">
-                    Hủy đặt lịch
-                </button>
-            )}
-        </section>
-    );
+  console.log(bookingHistory);
+  // console.log(booking.data.status);
+  const onChange: DatePickerProps['onChange'] = (date, dateString) => {
+    console.log(date, dateString);
+  };
+  return (
+    <section className="">
+      
+      <div className="">
+      <Space direction="vertical">
+        <DatePicker onChange={onChange} />
+
+      </Space>
+        <table className="font-inter w-full table-auto border-separate border-spacing-y-1 overflow-scroll text-left md:overflow-auto">
+          <thead className="w-full rounded-lg bg-gray-200 text-base font-semibold text-white">
+            <tr className="">
+              <th className="whitespace-nowrap rounded-l-lg py-3 pl-3 text-sm font-normal text-[#212B36]">Tên sân</th>
+              <th className="whitespace-nowrap py-3 pl-1 text-sm font-normal text-[#212B36]">Địa chỉ</th>
+              <th className="whitespace-nowrap py-3 text-sm font-normal text-[#212B36]">Ca sân</th>
+              <th className="whitespace-nowrap py-3 text-sm font-normal text-[#212B36]">Giờ sân</th>
+              <th className="whitespace-nowrap py-3 text-sm font-normal text-[#212B36]">Giá tiền</th>
+              <th className="whitespace-nowrap px-2.5 py-3 text-sm font-normal text-[#212B36]">Ngày đặt</th>
+
+              <th className="whitespace-nowrap rounded-r-lg py-3 pl-1 text-sm font-normal text-[#212B36]">Customer</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookingHistory?.data.map((booking: any) => {
+              return (
+                <tr key={booking._id} className="cursor-pointer bg-[#f6f8fa] drop-shadow-[0_0_10px_rgba(34,46,58,0.02)] hover:shadow-2xl">
+                  <td className="rounded-r-[8px] px-1 py-4 text-sm font-normal text-[#637381]">
+                    <div className="relative flex items-center gap-1">
+                      <div className="h-[50px] w-[50px]"><img src={booking.pitch?.avatar} alt="hepta-brown" className="min-h-[50px] min-w-[22px]" /></div>
+                      {booking.pitch?.name}
+                    </div>
+                  </td>
+                  <td className="rounded-l-lg py-4 pl-3 text-sm font-normal text-[#637381]">{booking.pitch?.address}</td>
+                  <td className="px-1 py-4 text-sm font-normal text-[#637381]"><span className="text-lime-800">{booking.shift?.time_start}</span>-<span className="text-green-800">{booking.shift?.time_end}</span></td>
+                  <td className="px-1 py-4 text-sm font-normal text-[#637381]">{booking.shift?.number_shift}</td>
+                  <td className="px-1 py-4 text-sm font-normal text-[#637381]">{booking.shift?.price}</td>
+
+
+                  <td className="px-1 py-4 text-sm font-normal text-[#637381]">{format(new Date(booking.updatedAt), "HH:mm:ss dd-MM-yyyy")}</td>
+                  <td className="px-1 py-4 " >
+
+
+                    <button onClick={handleCancelBooking} className={`p-4 bg-${booking.status === "success" ? "green-600" : "red-600"}`}>
+                      <span className="text-white">
+                        {booking.status === "success" ? "Đặt thành công" : "Bạn đã hủy lịch"}
+                      </span>
+                    </button>
+
+                  </td>
+                </tr>
+              )
+            })}
+
+          </tbody>
+        </table>
+      </div>
+      {/*           
+            <div className="">
+                
+                <img src={booking.data.pitch?.avatar} alt="" className="w-10" />
+                <p>adress: {booking.data.pitch?.address}</p>
+                
+            </div> */}
+
+
+    </section>
+  );
 };
 
 export default BookingHistoryItem;
--- src/pages/main/BookingHistory/index.tsx
+++ src/pages/main/BookingHistory/index.tsx
@@ -1,19 +1,14 @@
-import { useGetAllBookingByUserIdQuery } from "~/Redux/booking/bookingApi";
+
 import BookingHistoryItem from "~/components/BookingHistoryItem";
 
 const BookingHistoryPage = () => {
-    const userId = "65131393f8698962d691cd12";
-    const { data: bookingHistory } = useGetAllBookingByUserIdQuery({ user_id: userId });
 
     return (
-        <div className="max-w-[900px]">
-            <div className="">Lịch sử booking</div>
-
-            <div className="grid grid-cols-[1fr_300px]">
+        <div className="mt-24 m-10 overflow-hidden">
+            <h1 className="text-2xl font-body uppercase"  >Lịch sử booking</h1>
+            <div className="">
                 <div className="">
-                    {bookingHistory?.data.map((booking) => (
-                        <BookingHistoryItem key={booking._id} {...booking} />
-                    ))}
+                        <BookingHistoryItem  />
                 </div>
 
                 <div className=""></div>
