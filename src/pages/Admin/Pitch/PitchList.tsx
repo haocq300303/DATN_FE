a557f4fcf3e2f434824527f8263267f9602f3b34
hieutm1501 <hieuhop2003@gmail.com>
2023-12-12 00:41:15 +0700

    update du an

--- src/pages/Admin/Pitch/PitchList.tsx
+++ src/pages/Admin/Pitch/PitchList.tsx
@@ -5,7 +5,9 @@ import {
   Button,
   message,
   Input,
-  InputRef
+  InputRef,
+  Modal,
+  Image
 } from "antd";
 import type { ColumnsType, ColumnType } from "antd/es/table";
 import {
@@ -26,7 +28,7 @@ import { getAllServiceMid } from "~/Redux/Slices/serviceSlice";
 import Highlighter from 'react-highlight-words';
 import type { FilterConfirmProps } from 'antd/es/table/interface';
 import IFeedback from "~/interfaces/feedback";
-import { PitchPagination, getAllPitch } from "~/api/pitch";
+import { PitchPagination, getAllPitch, getOnePitch } from "~/api/pitch";
 
 
 interface DataType {
@@ -57,6 +59,11 @@ const PitchList = () => {
   const searchInput = useRef<InputRef>(null);
   const [totalItems, setTotalItems] = useState(Number);//phantrang
   const [currentPage, setCurrentPage] = useState(1);//phantrang
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedId, setSelectedId] = useState<string | null>(null);
+  const [Pitch, setPitch] = useState<any>({});
+
+
 
   const dispatch = useAppDispatch();
   const pitchs = useAppSelector((state) => state.pitch.pitchs);
@@ -96,6 +103,29 @@ const PitchList = () => {
     // window.scrollTo({ top: 500, behavior: 'smooth' });
   }
   // kết thức xử lí phân trang
+
+  const showModal = (idPitch: string) => {
+    setSelectedId(idPitch);
+    setIsModalOpen(true);
+  };
+
+  useEffect(() => {
+    // Gọi hàm getOnePitch khi component được render và selectedId thay đổi
+    if (selectedId) {
+      getOnePitch(selectedId).then(({ data: { data } }) => {
+        setPitch(data)
+      });
+    }
+  }, [selectedId]);
+  console.log("pitchModel:", Pitch);
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
   const confirm = async (idPost: string) => {
     await dispatch(fetchDeletePitch(idPost));
@@ -238,11 +268,17 @@ const PitchList = () => {
       width: '10%',
       render: (record) => (
         <Space size="middle">
+          <Button
+            className=" bg-blue-600" type="primary"
+            onClick={() => showModal(record?.key)}
+          >
+            Chi Tiết
+          </Button>
           <Popconfirm
             placement="topRight"
             title="Xóa bài viết?"
             description="Bạn có chắc chắn xóa bài viết này không?"
-            onConfirm={() => confirm(record._id)}
+            onConfirm={() => confirm(record.key)}
             onCancel={cancel}
             okText="Đồng ý"
             cancelText="Không"
@@ -273,7 +309,102 @@ const PitchList = () => {
           onChange: handlePageChange,
         }}
       />
+      <Modal
+        title="Chi Tiết Sân"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        width={1000}
+        footer={[
+          <Button key="back" onClick={handleCancel}>
+            Hủy
+          </Button>,
+        ]}
+      >
+        <div>
+          <div className=" mt-[10px]">
+            <div className=" ">
+              <div className="text-[18px] pt-[10px]">
+                <div className="flex justify-between">
+                  <p className="py-[5px]">Tên Sân : <span className="italic text-gray-700">{Pitch?.name}</span></p>
+                  <p className="py-[5px]">Chủ Sở Hữu : <span className="italic text-gray-700">{Pitch?.admin_pitch_id?.name || "Chủ Sân Bị Xoá"}</span></p>
+                </div>
+                <div className="flex justify-between">
+                  <p className="py-[5px]">Vị trí tìm kiếm : <span className="italic text-gray-700">{Pitch?.address}</span></p>
+                  <p className="py-[5px]">Số lượng sân : <span className="italic text-gray-700">{Pitch?.numberPitch} sân</span></p>
+                </div>
+                <div className="flex justify-between">
+                  <p className="py-[5px] w-[500px]">Dịch vụ :
+                    <span>
+                      {Pitch && Pitch?.services?.length > 0 ? (
+                        Pitch?.services?.map((item: any) =>
+                          <Button size={'small'} className="text-blue-500 mr-[5px]"> {item?.name} - {item?.price?.toLocaleString('vi-VN')}</Button>
+                        )
+                      ) : (
+                        <Button size={'small'} className="text-blue-500"> Chưa Có Dịch Vụ</Button>
+                      )}
+                    </span>
+                  </p>
+                  <p className="py-[5px]">Giá tiền giao động : <span className="text-red-400">{Pitch?.deposit_price?.toLocaleString('vi-VN')}₫ - 850.000₫</span></p>
+                </div>
+                <div className="flex justify-between">
+                  <p className="py-[5px]">Ngày tạo sân : <span className="italic text-gray-700">{Pitch?.createdAt}</span></p>
+                  <p className="py-[5px]">Ngày cập nhật sân gần nhất : <span className="italic text-gray-700">{Pitch?.updatedAt}</span></p>
+                </div>
+                <p className="py-[5px] w-[960px]">Miêu tả sân : <span className="italic w-[900px] text-gray-700 "> {Pitch?.description} Tổng bí thư, Chủ tịch Trung Quốc Tập Cận Bình sẽ thăm Việt Nam ngày 12-13/12, vào dịp hai nước kỷ niệm 15 năm thiết lập quan hệ Đối tác hợp tác Chiến lược Toàn diện. Đây là lần thứ ba ông Tập thăm Việt Nam ở cương vị người đứng đầu đảng và nhà nước Trung Quốc.</span></p>
+              </div>
+            </div>
+            <div className="pr-[20px]">
+              <div className="flex justify-center">
+                {Pitch?.images && Pitch.images.length > 0 && (
+                  <Image
+                    width={950}
+                    height={450}
+                    src={Pitch?.avatar}
+                    preview={{
+                      src: `${Pitch?.avatar}`,
+                    }}
+                  />
+                )}
+              </div>
+              <div className="flex justify-between mt-[10px]">
+                {Pitch?.images && Pitch.images.length > 0 && (
+                  <Image
+                    width={300}
+                    height={150}
+                    src={Pitch?.images[0]}
+                    preview={{
+                      src: `${Pitch?.images[0]}`,
+                    }}
+                  />
+                )}
+                {Pitch?.images && Pitch.images.length > 0 && (
+                  <Image
+                    width={300}
+                    height={150}
+                    src={Pitch?.images[1]}
+                    preview={{
+                      src: `${Pitch?.images[1]}`,
+                    }}
+                  />
+                )}
 
+                {Pitch?.images && Pitch.images.length > 0 && (
+                  <Image
+                    width={300}
+                    height={150}
+                    src={Pitch?.images[2]}
+                    preview={{
+                      src: `${Pitch?.images[2]}`,
+                    }}
+                  />
+                )}
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </Modal>
 
 
     </>
